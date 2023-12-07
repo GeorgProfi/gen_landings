36e50cb793af8bf0f33533d897b0ac456a234c9a
Errormacr <tulupov.valeriy@mail.ru>
2023-12-07 14:04:20 +0300

    Drag and drop

--- lib/main.dart
+++ lib/main.dart
@@ -13,23 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Generation Landings',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 80, 68, 255)),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 80, 68, 255)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Generation Landings'),
@@ -39,16 +24,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -56,32 +31,93 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<Widget> header = [];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    addToHeader(widgetToAdd) {
+      setState(() {
+        header.add(widgetToAdd);
+      });
+    }
+
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: const Center(
-          child: Text(
-        '',
-      )),
-
-      // This trailing comma makes auto-formatting nicer for build methods.
+      body: Stack(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    DragTarget<Widget>(
+                      builder: (context, candidateItems, rejectedItems) {
+                        return Container(
+                          color: Colors.green,
+                          height: screenHeight * 0.17,
+                        );
+                      },
+                      onAccept: (item) {
+                        addToHeader(item);
+                      },
+                    ),
+                    Container(
+                      color: const Color.fromARGB(255, 85, 21, 21),
+                      height: screenHeight * 0.6,
+                    ),
+                    Container(
+                      color: const Color.fromARGB(255, 175, 76, 142),
+                      height: screenHeight * 0.17,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            right: 1,
+            child: Container(
+              color: Colors.blue,
+              width: 150,
+              height: screenHeight,
+              child: SingleChildScrollView(
+                  child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Column(children: [
+                  LongPressDraggable<Widget>(
+                    data: Container(
+                      width: 100,
+                      height: 50,
+                      color: Colors.white,
+                      child: const Center(child: Text('Text')),
+                    ),
+                    child: Container(
+                      width: 100,
+                      height: 50,
+                      color: Colors.white,
+                      child: const Center(child: Text('Text')),
+                    ),
+                    feedback: Material(
+                      elevation: 5,
+                      child: Container(
+                        width: 100,
+                        height: 50,
+                        color: Colors.white,
+                        child: const Center(child: Text('Text')),
+                      ),
+                    ),
+                    childWhenDragging: Container(),
+                  ),
+                ]),
+              )),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
